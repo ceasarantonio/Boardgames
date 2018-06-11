279e65f3de58d9e3a637e8e0afb2268469981853
Daniel Karlsson (sia568) <daniel.w.karlsson@teliacompany.com>
2018-06-11 12:19:51 +0200

    fix some left over code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDROMKVCtHaikNn3wZrAwtbEh8KkaNuT7w",
+    authDomain: "board-games-d912d.firebaseapp.com",
+    databaseURL: "https://board-games-d912d.firebaseio.com",
+    projectId: "board-games-d912d",
+    storageBucket: "board-games-d912d.appspot.com",
+    messagingSenderId: "14332721887"
+  }
 };
